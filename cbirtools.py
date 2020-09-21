7a92b4b8477e97c938f4182569397eb53e7ee0fc
DgrinderHZ <hassanzekkouri@gmail.com>
2020-09-21 15:51:30 +0100

    Added HSV Histogram

--- cbirtools.py
+++ cbirtools.py
@@ -64,6 +64,60 @@ class ColorDescriptor():
                             ranges=[0, 256, 0, 256, 0, 256])
 		
         return rbgHist
+    
+    def getHistHSV(self, image, bins):
+        """
+
+        Credits to:
+            https://www.pyimagesearch.com/2014/12/01/complete-guide-building-image-search-engine-python-opencv/
+        """
+		# convert the image to the HSV color space and initialize
+		# the features used to quantify the image
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		features = []
+		# grab the dimensions and compute the center of the image
+		(h, w) = image.shape[:2]
+		(cX, cY) = (int(w * 0.5), int(h * 0.5))
+        # divide the image into four rectangles/segments (top-left,
+		# top-right, bottom-right, bottom-left)
+		segments = [(0, cX, 0, cY), (cX, w, 0, cY), (cX, w, cY, h),
+			(0, cX, cY, h)]
+		# construct an elliptical mask representing the center of the
+		# image
+		(axesX, axesY) = (int(w * 0.75) // 2, int(h * 0.75) // 2)
+		ellipMask = np.zeros(image.shape[:2], dtype = "uint8")
+		cv2.ellipse(ellipMask, (cX, cY), (axesX, axesY), 0, 0, 360, 255, -1)
+		# loop over the segments
+		for (startX, endX, startY, endY) in segments:
+			# construct a mask for each corner of the image, subtracting
+			# the elliptical center from it
+			cornerMask = np.zeros(image.shape[:2], dtype = "uint8")
+			cv2.rectangle(cornerMask, (startX, startY), (endX, endY), 255, -1)
+			cornerMask = cv2.subtract(cornerMask, ellipMask)
+			# extract a color histogram from the image, then update the
+			# feature vector
+			hist = self.histogram(image, cornerMask, bins)
+			features.extend(hist)
+		# extract a color histogram from the elliptical region and
+		# update the feature vector
+		hist = self.histogram(image, ellipMask, bins)
+		features.extend(hist)
+		# return the feature vector
+		return features
+
+    def histogram(self, image, mask, bins):
+		# extract a 3D color histogram from the masked region of the
+		# image, using the supplied number of bins per channel
+		hist = cv2.calcHist([image], [0, 1, 2], mask, self.bins,
+			[0, 180, 0, 256, 0, 256])
+		# normalize the histogram if we are using OpenCV 2.4
+		if imutils.is_cv2():
+			hist = cv2.normalize(hist).flatten()
+		# otherwise handle for OpenCV 3+
+		else:
+			hist = cv2.normalize(hist, hist).flatten()
+		# return the histogram
+		return hist
 
     #_________________________________________________________________________
 class TextureDescriptor:
@@ -179,6 +233,13 @@ class Distance():
         h1 = obj[1]
         h2 = query[1]
         return cv2.compareHist(h1, h2, cv2.HISTCMP_CHISQR)
+    
+    def chi2_distance(self, histA, histB, eps = 1e-10):
+		# compute the chi-squared distance
+		d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
+			for (a, b) in zip(histA, histB)])
+		# return the chi-squared distance
+		return d
 
     def intersect(obj, query):
         h1 = obj[1]
